48c402a537aa5dc7f0309aed29e1adb7a233b186
Rename0209 <rename0209@gmail.com>
2025-03-21 17:03:29 +0700

    cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { saveSession, getSession } from './src/sessionService.js';
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 const app = express();
 app.use(express.json());
